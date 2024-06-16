df2f703672b004935c9befcca5c4c9900e187fb4
Stanislav <st.korshunov88@gmail.com>
2024-06-16 13:30:14 +0300

    add class test UI in project

--- src/test/java/com/itacademy/TestOnSeleniumUi.java
+++ src/test/java/com/itacademy/TestOnSeleniumUi.java
@@ -1,9 +1,48 @@
 package com.itacademy;
 
 import com.itacademy.testngexample.LoggerTest;
-import org.apache.log4j.LogManager;
-import org.apache.log4j.Logger;
+import com.itacademy.utils.pages.MainPage;
 
-public class testOnSeleniumUi {
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+
+public class TestOnSeleniumUi extends BaseTest {
     private static final Logger LOGGER = LogManager.getLogger(LoggerTest.class);
+
+
+    @Test
+    public void firstTest() throws InterruptedException {
+        MainPage mainPage = new MainPage(driver);
+        mainPage.openUrl();
+        mainPage.clickAccountButton();
+        mainPage.typeLogin("st.korshunov88@gmail.com");
+        mainPage.clickButtonContinue();
+        mainPage.typeCodeInputField("564895");
+        Thread.sleep(10000);
+        mainPage.clickButtonSubmit();
+        Thread.sleep(10000);
+        String string = mainPage.getIsErrorPresent();
+        Assert.assertEquals(mainPage.getIsErrorPresent(), "Sent to st.korshunov88@gmail.com");
+        LOGGER.info("info");
+    }
+
+    @Test
+    public void secondTest() throws InterruptedException {
+        MainPage mainPage = new MainPage(driver);
+        mainPage.openUrl();
+        mainPage.clickButtonCloseDialog();
+        mainPage.clickButtonEquipment();
+        mainPage.scrollPage();
+        String str = mainPage.getTextWtRiSeT();
+        Thread.sleep(5000);
+        mainPage.clickButtonRingsSet();
+        mainPage.clickButtonAddToBasket();
+        String str2 = mainPage.getTextOneProductToBasket();
+        Assert.assertEquals(str,str2,"Workout Rings Set");
+        LOGGER.info("info");
+    }
 }
