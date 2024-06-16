b49f372dd06f604025628e8fe8639b93a95010a7
Stanislav <st.korshunov88@gmail.com>
2024-06-16 13:41:15 +0300

    add class BaseTest

--- src/test/java/com/itacademy/BaseTest.java
+++ src/test/java/com/itacademy/BaseTest.java
@@ -1,14 +1,17 @@
-package com.itacademy.utils;
+package com.itacademy;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
+import java.util.concurrent.TimeUnit;
 
 
-public class DriverManager {
-    protected WebDriver driver;
+public class BaseTest {
+    static WebDriver driver;
 
 
     @BeforeMethod
@@ -16,7 +19,9 @@ public class DriverManager {
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\stkor\\chromedriver.exe");
         driver = new ChromeDriver();
         driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+
 
     }
 
