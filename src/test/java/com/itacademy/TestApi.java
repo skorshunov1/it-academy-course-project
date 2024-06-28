38450f422044a36bc7776634cf119bc6be6fe6db
Stanislav <st.korshunov88@gmail.com>
2024-06-28 12:18:20 +0300

    add four test Api post,get,post,delete

--- src/test/java/com/itacademy/TestApi.java
+++ src/test/java/com/itacademy/TestApi.java
@@ -21,20 +21,50 @@ public class TestApi {
 
 
     @Test
-    public void getTest() {
+    public void postLoginRequest(){
         given().log().all()
-                .when().get("https://www.21vek.by/special_offers/promo.html?discountTypes=sale")
+                .header("content-type", "application/json")
+                .body("{ 'login': 'stkorshunov^@gmail.com', 'password': 'qSCNd89sd^@'}")
+                .when().post("https://catalog.onliner.by/sdapi/user.api/login")
+                .then().log().all().statusCode(400);
+
+    }
+
+    @Test
+    public void getRequest() {
+        given().log().all()
+                .when().get("https://catalog.onliner.by/sdapi/catalog.api/products/majorivbtblk?include=schema")
                 .then().log().all().statusCode(200);
     }
 
     @Test
-    public void postTest(){
+    public void postRequest(){
+       File file = new File("src/test/resources/json/body.json");
         given().log().all()
-                .header("Cookie", "21vek=fca38344-78ef-490e-92c1-6dc958006ae8")
+                .header("Cookie", "stid=bf438997757c3170faa282ebf68d23c08a2a6ca16bea67f835dc8f31a17b111f")
                 .header("content-type", "application/json")
-                .body("{\"User\":{\"email\":\"dfg@gmail.com\",\"password\":\"fdg15d33\"}}")
-                .when().post("https://www.21vek.by/users/action/login/")
+                .body(file)
+                .when().post("https://catalog.onliner.by/sdapi/cart.api/detached-cart/1affd58a-3528-11ef-9174-0242567289e1/add/")
                 .then().log().all().statusCode(200);
     }
 
+
+    @Test
+    public void deleteRequest(){
+        given().log().all()
+                .header("Cookie", "stid=bf438997757c3170faa282ebf68d23c08a2a6ca16bea67f835dc8f31a17b111f")
+                .header("content-type", "application/json")
+                .body("{\n" +
+                        "  \"positions\": [\n" +
+                        "   {\n" +
+                        "   \"position_id\": \"6498:859088\",\n" +
+                        "   \"shop_id\": 6498,\n" +
+                        "   \"product_id\": 2202506\n" +
+                        "   }\n" +
+                        "   ]\n" +
+                        "}")
+                .when().delete("https://cart.onliner.by/sdapi/cart.api/detached-cart/1affd58a-3528-11ef-9174-0242567289e1")
+                .then().log().all().statusCode(204);
+
+    }
 }
