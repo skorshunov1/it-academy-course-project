1228b7ae3aedb4e446a31127ea52e4923439da7f
Stanislav <st.korshunov88@gmail.com>
2024-06-29 14:53:49 +0300

    add listeners on class BaseTest

--- src/test/java/com/itacademy/BaseTest.java
+++ src/test/java/com/itacademy/BaseTest.java
@@ -1,15 +1,16 @@
 package com.itacademy;
 
+import com.itacademy.utils.pages.listeners.TestListeners;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Listeners;
 
 import java.util.concurrent.TimeUnit;
 
-
+@Listeners(TestListeners.class)
 public class BaseTest {
     static WebDriver driver;
 
