c7bdbfcd9624fe9f326402fbd8acc346f6e178b0
Stanislav <st.korshunov88@gmail.com>
2024-06-29 14:52:29 +0300

    add class listeners

--- src/main/java/com/itacademy/utils/pages/listeners/TestListeners.java
+++ src/main/java/com/itacademy/utils/pages/listeners/TestListeners.java
@@ -1,2 +1,34 @@
-package com.itacademy.utils.pages.listeners;public class TestListeners {
+package com.itacademy.utils.pages.listeners;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+
+
+public class TestListeners implements ITestListener {
+    private static final Logger LOGGER = LogManager.getLogger(TestListeners.class);
+
+    @Override
+    public void onTestStart(ITestResult result) {
+        LOGGER.info("#######################################################");
+        LOGGER.info("Test " + result.getMethod().getMethodName()+ " started");
+        LOGGER.info("#######################################################");
+    }
+
+    @Override
+    public void onTestSuccess(ITestResult result) {
+        LOGGER.info("#######################################################");
+        LOGGER.info("Test " + result.getMethod().getMethodName()+ " success");
+        LOGGER.info("#######################################################");
+    }
+
+    @Override
+    public void onTestFailure(ITestResult result) {
+        LOGGER.info("#######################################################");
+        LOGGER.info("Test " + result.getMethod().getMethodName()+ " failure");
+        LOGGER.info("#######################################################");
+    }
 }
+
