826e92d8caf8ca984783a9bb17b28b03d08c8b7a
Stanislav <st.korshunov88@gmail.com>
2024-07-05 19:44:59 +0300

    add method waiters waitElementClicable

--- src/main/java/com/itacademy/waiters/Waiters.java
+++ src/main/java/com/itacademy/waiters/Waiters.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.sql.DriverManager;
 import java.time.Duration;
 
 public class Waiters {
@@ -12,6 +13,13 @@ public class Waiters {
     public static WebElement clickWebElement(WebElement webElement,WebDriver driver) {
         new WebDriverWait(driver, Duration.ofSeconds(10)).until(ExpectedConditions.visibilityOf(webElement));
         return webElement;
+    }
+    public static WebElement waitElementClickable(WebElement element, WebDriver driver){
+        new WebDriverWait(driver,Duration.ofSeconds(5))
+                .until(ExpectedConditions.elementToBeClickable(element));
+        return element;
+
+
     }
 
 
