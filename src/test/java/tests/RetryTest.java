a4e3c58c4b535f170c1916f26ead0ee9a96d8d86
Stanislav <st.korshunov88@gmail.com>
2024-07-11 12:38:55 +0300

    add class RetryTest

--- src/test/java/tests/RetryTest.java
+++ src/test/java/tests/RetryTest.java
@@ -1,2 +1,20 @@
-package tests;public class RetryTest {
+package tests;
+
+import org.testng.IRetryAnalyzer;
+import org.testng.ITestResult;
+
+public class RetryTest implements IRetryAnalyzer {
+
+    int initCount = 0;
+    int maxRetryCount = 3;
+
+    @Override
+    public boolean retry(ITestResult result) {
+        if (initCount < maxRetryCount) {
+            initCount++;
+            return true;
+        }
+        return false;
+    }
+
 }
